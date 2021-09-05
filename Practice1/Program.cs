76e26aaba0201155eec921b653bba2f89be88347
pooja-landage <poojalandage22@gmail.com>
2021-09-05 12:08:42 +0530

    Add project files.

---
+++ Practice1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a =  23;
+            b = 34;
+            Console.WriteLine("the value of a is {0}", a);
+            Console.WriteLine("the value of b is {0}",  + b);
+
+            int num1, num2;
+            num1 = num2 = 24;
+            Console.WriteLine("The value of first variable is {0} ", num1);
+            Console.WriteLine("The value of second variable is " + num2);
+
+
+
+
+
+        }
+    }
+}
